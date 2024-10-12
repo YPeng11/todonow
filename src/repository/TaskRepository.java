db8b191dff936fb6149f2f69e2bb52b84f6f6c1e
YPeng11 <161717186+YPeng11@users.noreply.github.com>
2024-10-13 03:01:24 +0800

    Update TaskRepository.java
    TaskRepository is used to perform basic database operations such as save, update, and delete tasks.

--- src/repository/TaskRepository.java
+++ src/repository/TaskRepository.java
@@ -1,4 +1,4 @@
-package   包 com.todonow.repository;
+package com.todonow.repository;
 
 import com.todonow.model.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -7,4 +7,3 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface TaskRepository extends JpaRepository<Task, Long> {
 }
-
